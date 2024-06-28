e778adf14941dc96accb6f8e2412253968a36afa
BosEriko <bos.eriko@gmail.com>
2024-06-28 19:32:04 +0800

    add sudo to all install script

--- .files/install/entry.sh
+++ .files/install/entry.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
 
-~/.files/install/modules/apt.sh
-~/.files/install/modules/fzf.sh
-~/.files/install/modules/youtube-dl.sh
-~/.files/install/modules/node.sh
-~/.files/install/modules/docker.sh
-~/.files/install/modules/ruby.sh
-~/.files/install/modules/heroku.sh
-~/.files/install/modules/mkdir.sh
-~/.files/install/modules/zsh.sh
-~/.files/install/modules/pull.sh
-~/.files/install/modules/origin.sh
-~/.files/install/modules/ssh.sh
-~/.files/install/modules/reset.sh
+sudo ~/.files/install/modules/apt.sh
+sudo ~/.files/install/modules/fzf.sh
+sudo ~/.files/install/modules/youtube-dl.sh
+sudo ~/.files/install/modules/node.sh
+sudo ~/.files/install/modules/docker.sh
+sudo ~/.files/install/modules/ruby.sh
+sudo ~/.files/install/modules/heroku.sh
+sudo ~/.files/install/modules/mkdir.sh
+sudo ~/.files/install/modules/zsh.sh
+sudo ~/.files/install/modules/pull.sh
+sudo ~/.files/install/modules/origin.sh
+sudo ~/.files/install/modules/ssh.sh
+sudo ~/.files/install/modules/reset.sh
